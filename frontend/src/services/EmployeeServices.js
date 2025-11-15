749916d0f1eb9c4bded028d0cae89d78db0d7c6b
Beatriz Vitória <beatrizvitoriabrandaosilva1412@gmail.com>
2025-11-15 16:11:35 -0300

    Desenvolvimento do arquivo 'EmployeeService.js'

--- frontend/src/services/EmployeeServices.js
+++ frontend/src/services/EmployeeServices.js
@@ -4,3 +4,72 @@
  * description: arquivo responsável pelos métodos de requisições das Apis via HTTP
  * author: Beatriz Vitória <beatrizvsbrandao@gmail.com>
  */
+
+import Api from "./Api";
+
+export default {
+    /**
+     * Método responsável por criar um novo 'Employee'
+     * (POST): localhost:3000/api/employees
+     */
+    async createNewEmployee(employee) {
+        try {
+            const response = await Api().post(('/employees', employee));
+            return response.data;
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    /**
+     * Método responsável por listar todos os 'Employees'
+     * (GET): localhost:3000/api/employees
+     */
+    async getEmployees() {
+        try {
+            const response = await Api().get(('/employees'));
+            return response.data;
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    /**
+     * Método responsável por listar por Id um determinado 'Employee'
+     * (GET): localhost:3000/api/employees/:id
+     */
+    async getEmployeesId(id) {
+        try {
+            const response = await Api().get((`/employees/${id}`));
+            return response.data;
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    /**
+     * Método responsável por atualizar um determinado 'Employee' por Id
+     * (PUT): localhost:3000/api/employees/:id
+     */
+    async updateEmployee(id) {
+        try {
+            const response = await Api().put((`/employee/${id}`));
+            return response.data;
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    /**
+     * Método responsável por deletar um determinado 'Employee' por Id
+     * (DELETE): localhost:3000/employees/:id
+     */
+    async deleteEmployee(id) {
+        try {
+            const response = await Api().delete((`/employees/${id}`));
+            return response.data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
