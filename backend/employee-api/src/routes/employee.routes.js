871696163a13c8b740bafdc1638d256344d8faf0
Beatriz Vitória <beatrizvitoriabrandaosilva1412@gmail.com>
2025-11-15 13:30:30 -0300

    Desenvolvimento da rota 'updateEmployeeById'

--- backend/employee-api/src/routes/employee.routes.js
+++ backend/employee-api/src/routes/employee.routes.js
@@ -19,4 +19,7 @@ router.get('/employees', employeeController.listAllEmployees);
 // ==> Rota responsável por listar um determinado'Funcionário' por Id: (GET): localhost:3000/api/employees/:id
 router.get('/employees/:id', employeeController.findEmployeeById);
 
+// ==> Rota responsável por atualizar informações de um determinado 'Funcionário' por Id: (PUT): localhost:3000/api/employees/:id
+router.put('/employees/:id', employeeController.updateEmployeeById);
+
 module.exports = router;
