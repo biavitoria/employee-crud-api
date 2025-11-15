8b23b68f91b7ede2a95b74e3e64ef07d02c867ff
Beatriz Vitória <beatrizvitoriabrandaosilva1412@gmail.com>
2025-11-15 15:34:46 -0300

    Desenvolvimento do arquivo Api.js

---
+++ frontend/src/services/EmployeeServices.js
@@ -0,0 +1,6 @@
+/**
+ * file: src/services/EmployeeServices.js
+ * data: 15/11/2025
+ * description: arquivo responsável pelos métodos de requisições das Apis via HTTP
+ * author: Beatriz Vitória <beatrizvsbrandao@gmail.com>
+ */
