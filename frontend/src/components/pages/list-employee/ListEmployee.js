ab4aa545574a1c192e485daa547b9e9b455b4c35
Beatriz Vitória <beatrizvitoriabrandaosilva1412@gmail.com>
2025-11-16 15:43:26 -0300

    Desenvolvimento e desacoplamento da lógica do componente 'ListEmployeeComponent.vue'

--- frontend/src/components/pages/list-employee/ListEmployee.js
+++ frontend/src/components/pages/list-employee/ListEmployee.js
@@ -23,5 +23,29 @@ export default {
       const response = await EmployeeServices.getEmployees();
       this.employees = response;
     },
+
+    async removeEmployee(id) {
+		  this.$swal({
+			  title: 'Are you sure you want to remove the employee?',
+			  text: 'Watch out! This employee will be deleted',
+			  icon: 'warning',
+			  showConfirmButton: true,
+			  allowOutsideClick: false,
+			  allowEnterKey: true,
+			  allowEscapeKey: false,
+			  showCancelButton: true,
+			  confirmButtonColor: '#3085d6',
+			  cancelButtonColor: '#d33',
+			  confirmButtonText: 'Yes! Please, delete it!',
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          await EmployeeServices.deleteEmployee(id);
+          this.$swal('Deleted', 'Successfully delete', 'success');
+          this.listAllEmployees();
+        } else if (result.dismiss) {
+          this.$swal('Cancelled', 'Cancel deletion', 'info');
+        }
+      });
+    },
   },
 };
--- frontend/src/components/pages/list-employee/ListEmployeeComponent.vue
+++ frontend/src/components/pages/list-employee/ListEmployeeComponent.vue
@@ -27,7 +27,7 @@
               </router-link>
             </td>
             <td>
-              <button class="btn btn-danger">
+              <button @click="removeEmployee(employee.employee_id)" class="btn btn-danger">
                 <font-awesome-icon :icon="['fas', 'trash']"/> Delete
               </button>
             </td>
